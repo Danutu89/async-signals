b06b2a71836ec3b2dfe1e96efaae1c0d7abf81ba
Danutu89 <danutu@newapp.nl>
2020-11-23 11:25:46 +0200

    Fix

--- src/async_signals/dispatcher.py
+++ src/async_signals/dispatcher.py
@@ -12,16 +12,16 @@ class AsyncSignal(Signal):
         super(AsyncSignal, self).__init__(providing_args=providing_args)
         self.queue = queue
 
-    def send(self, sender, instance, action):
+    def send(self, sender, instance_id, action):
         """Send the signal via Celery."""
 
         self.propagate_signal.apply_async(
-            args=(sender, instance, action),
+            args=(self, sender, instance_id, action),
             queue=self.queue,
         )
 
     @task
-    def propagate_signal(self, sender, instance, action):
+    def propagate_signal(self, sender, instance_id, action):
         """
         Send signal from sender to all connected receivers catching errors.
 
@@ -48,6 +48,6 @@ class AsyncSignal(Signal):
         for receiver in self._live_receivers(_make_id(sender)):
             try:
                 receiver(signal=self, sender=sender,
-                         instance=instance, action=action)
+                         instance_id=instance_id, action=action)
             except Exception:
                 pass
